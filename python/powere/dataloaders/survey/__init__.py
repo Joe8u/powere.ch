90261fcbb47c1eb6f0aefd593790b91409211d85
joe8u <joe.kovatsch@gmail.com>
2025-08-17 19:30:01 +0200

    feat(survey): add back-compat aliases (q10_incentive_wide, q13_income, join_demographics)

--- python/powere/dataloaders/survey/__init__.py
+++ python/powere/dataloaders/survey/__init__.py
@@ -21,4 +21,22 @@ __all__ = [
     "load_incentives", "load_question_10_incentives",
     "load_nonuse", "load_question_9_nonuse",
     "load_socioeconomics", "load_income", "load_education", "load_party_pref",
+    # Back-compat für älteren API-Code:
+    "q10_incentive_wide", "q13_income", "join_demographics",
 ]
+
+# --- Back-compat Wrapper ---
+def q10_incentive_wide():
+    return load_incentives()
+
+def q13_income():
+    return load_income()
+
+def join_demographics():
+    import pandas as pd
+    from functools import reduce
+    dfs = load_demographics()
+    frames = [df for df in dfs.values() if not df.empty]
+    if not frames:
+        return pd.DataFrame()
+    return reduce(lambda l, r: l.merge(r, on="respondent_id", how="outer"), frames)
