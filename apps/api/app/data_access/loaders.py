f90274c1217449b606b7e4d89d8b7c1e69f4455c
joe8u <joe.kovatsch@gmail.com>
2025-08-17 12:14:48 +0200

    feat(dataloaders): survey Q1–Q15 loader + exports; ignore egg-info

---
+++ apps/api/app/data_access/loaders.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+from typing import List, Optional
+from powere.dataloaders.lastprofile import list_appliances, load_range
+from powere.dataloaders.survey import join_demographics, q13_income
---
+++ python/powere/dataloaders/survey/__init__.py
@@ -0,0 +1,12 @@
+from .io import (
+    q1_age, q2_gender, q3_household_size, q4_accommodation, q5_electricity,
+    q6_challenges, q7_consequence, q8_importance_wide, q9_nonuse_wide,
+    q10_incentive_wide, q11_notify_optin, q12_smartplug, q13_income,
+    q14_education, q15_party, join_demographics
+)
+__all__ = [
+    "q1_age","q2_gender","q3_household_size","q4_accommodation","q5_electricity",
+    "q6_challenges","q7_consequence","q8_importance_wide","q9_nonuse_wide",
+    "q10_incentive_wide","q11_notify_optin","q12_smartplug","q13_income",
+    "q14_education","q15_party","join_demographics"
+]
