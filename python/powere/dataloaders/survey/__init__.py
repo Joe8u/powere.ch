978332e9c21a2d66e834117ec06fbb5ac2fe2e45
joe8u <joe.kovatsch@gmail.com>
2025-08-17 17:34:08 +0200

    fix(dataloaders/survey): clean __init__ to export attitudes only

--- python/powere/dataloaders/survey/__init__.py
+++ python/powere/dataloaders/survey/__init__.py
@@ -1,28 +1,8 @@
-from .io import (
-    q1_age, q2_gender, q3_household_size, q4_accommodation, q5_electricity,
-    q6_challenges, q7_consequence, q8_importance_wide, q9_nonuse_wide,
-    q10_incentive_wide, q11_notify_optin, q12_smartplug, q13_income,
-)
+"""Survey dataloaders."""
+from .attitudes import load_attitudes, load_challenges, load_consequence
+
 __all__ = [
-    "q1_age","q2_gender","q3_household_size","q4_accommodation","q5_electricity",
-    "q6_challenges","q7_consequence","q8_importance_wide","q9_nonuse_wide",
-    "q10_incentive_wide","q11_notify_optin","q12_smartplug","q13_income",
+    "load_attitudes",
+    "load_challenges",
+    "load_consequence",
 ]
-
-from .attitudes import load_attitudes, load_challenges, load_consequence  # Q6/Q7
-
-try:
-    __all__.extend(["load_attitudes", "load_challenges", "load_consequence"])
-except NameError:  # falls __all__ oben nicht definiert wäre
-    __all__ = ["load_attitudes", "load_challenges", "load_consequence"]
-
-)
-try:
-    __all__.extend([
-    ])
-except NameError:
-    __all__ = [
-    ]
-
-try:
-except NameError:
